463a9d89a8f638623c7148aa78a2cf98ed8db851
camilo <camilosolquiroga@gmail.com>
2022-10-04 16:49:52 -0300

    prueba de funcionalidad de la tienda

--- frontend/src/js/tienda/printCarrito.js
+++ frontend/src/js/tienda/printCarrito.js
@@ -1,6 +1,6 @@
-import formatAmount from "./formatAmount";
-import storage from "./storage";
-import totalCarrito from "./totalCarrito";
+import formatAmount from "./formatAmount.js";
+import storage from "./storage.js";
+import totalCarrito from "./totalCarrito.js";
 
 export default function printCarrito() {
   const productCarrito = document.getElementById("productos-carrito");
